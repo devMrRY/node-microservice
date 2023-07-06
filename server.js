b872dad295139d67ede0eb81e07b2a7ecda51393
RAHUL YADAV <yadav.rahul2@coxautoinc.com>
2023-07-06 14:23:08 +0530

    update: added cors options

--- server.js
+++ server.js
@@ -1,44 +1,69 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cluster = require('cluster');
-const os = require('os');
+const cluster = require("cluster");
+const os = require("os");
 const PORT = 5000;
-const cors = require('cors');
-const EventEmitter = require('events');
+const cors = require("cors");
+const EventEmitter = require("events");
 const ev = new EventEmitter();
-app.use(cors());
+const corsOption = {
+  credentials: true,
+  origin: "*",
+  optionSuccessStatus: 200,
+  method: "GET, POST, PUT, DELETE",
+};
+app.use(cors(corsOption));
+process.on("unhandledRejection", (err, data) => {
+  console.log("unhandled rejection occured");
+  console.error(err ? err.message : err);
+});
 
-process.on('unhandledRejection', (err, data) => {
-    console.log('unhandled rejection occured')
-    console.error(err ? err.message : err)
-})
-
-app.get('/server/health', (req, res) => {
-    res.send(`pre-commit hook added server is running ${process.pid}`);
-})
+app.get("/server/health", (req, res) => {
+  res.cookie("webToken", "rahultest", {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    maxAge: 1000 * 60 * 60 * 24,
+  });
+  res.send(`latest server is running ${process.pid}`);
+});
 
 app.set("ev", ev);
 process.app = app;
 
-app.get('/test/multi_process', require('./controllers/multiProcess').multiProcess)
-app.get('/test/worker_thread', require('./controllers/workerThread').workerThread)
-app.get('/test/single_process', require('./controllers/singleProcess').singleProcess)
-app.get('/testEvent', require('./services/eventEmitter/event-emitter').testEvent);
+app.get(
+  "/test/multi_process",
+  require("./controllers/multiProcess").multiProcess
+);
+app.get(
+  "/test/worker_thread",
+  require("./controllers/workerThread").workerThread
+);
+app.get(
+  "/test/single_process",
+  require("./controllers/singleProcess").singleProcess
+);
+app.get(
+  "/testEvent",
+  require("./services/eventEmitter/event-emitter").testEvent
+);
 
-if(cluster.isMaster){
-    let nCPUs = os.cpus().length;
-    for(let i=0; i<nCPUs; i++){
-        let worker = cluster.fork();
-    }
-}else{
-    const server = app.listen(PORT, () => {
-        console.log(`server is listening with process ${process.pid} on port ${PORT}`)
-    })
+if (cluster.isMaster) {
+  let nCPUs = os.cpus().length;
+  for (let i = 0; i < nCPUs; i++) {
+    let worker = cluster.fork();
+  }
+} else {
+  const server = app.listen(PORT, () => {
+    console.log(
+      `server is listening with process ${process.pid} on port ${PORT}`
+    );
+  });
 
-    process.on('SIGTERM', () => {
-        server.close(() => {
-            console.log('server is closed');
-            process.exit(0);
-        })
-    })
+  process.on("SIGTERM", () => {
+    server.close(() => {
+      console.log("server is closed");
+      process.exit(0);
+    });
+  });
 }
